d4025db9523afc48cd629724b54615950dde4524
Michael Burke <mike@pixotech.com>
2016-09-27 14:59:51 -0500

    Delete some test files. Update accessibility_validator to grab json data from test and save it as a ruby hash and as json for easy use later.

--- accessibility_validator.rb
+++ accessibility_validator.rb
@@ -7,102 +7,43 @@ require 'open-uri'
 require 'pry'
 require 'sudo'
 
+require_relative 'email_reports'
 
+title = 'test'
+# Chrome browser instantiation
+driver = Selenium::WebDriver.for :chrome
 
+# Navigate to accessibility checker
+driver.navigate.to "http://fae20.cita.illinois.edu/login/"
 
+driver.find_element(:id, 'id_username').send_keys('pixo.qa')
+driver.find_element(:id, 'id_password').send_keys('P9cTntdXK2')
+driver.find_element(:css, "input[type='submit']").click
 
+# Enter info and click
+driver.find_element(:id, 'id_input_url').send_keys('www.google.com')
+driver.find_element(:id, 'id_input_title').send_keys(title)
+driver.find_element(:id, 'depth_1').click
+driver.find_element(:id, 'id_submit').click
 
+# Wait for processing to finish before going to Archived Reports
+wait = Selenium::WebDriver::Wait.new(timeout: 60)
+begin
+  element = wait.until { driver.find_element(:link_text => title) }
+end
+driver.find_element(:link_text => 'Archived Reports').click
+json_link = driver.find_element(:css, "#id_table_reports tbody tr:first-of-type td:last-of-type a").attribute("href")
 
+# Read json from link and write to Ruby hash
+returned_data = JSON.parse(open(json_link) { |f| f.read })
 
-# uri = URI.parse("https://fae.disability.illinois.edu/")
+# Make pretty JSON
+json_data = JSON.pretty_generate(returned_data)
+puts json_data
 
-# http = Net::HTTP.post_form(uri, {
-    # "url" => "http://www.google.com",
-    # "title" => "test"
-#   });
-# request = Net::HTTP::Get.new(uri.request_uri)
+# Quitting the browser
+driver.quit
 
-# response = http.request(request)
-
-# http = Net::HTTP.new(uri.host, uri.port)
-
-# request = Net::HTTP::Post.new(uri.request_uri)
-# request.add_field("Referer", "https://fae.disability.illinois.edu/")
-# request.add_field("Debug", "true")
-# request.set_form_data({
-#   "url" => "http://www.google.com",
-#   "title" => "test",
-# });
-# request.basic_auth("qa@pixotech.com", "P9cTntdXK2")
-# http.use_ssl = true
-# http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-# response = http.request(request);
-
-
-# puts response.body
-
-
-
-# uri = URI('https://fae.disability.illinois.edu/')
-# res = Net::HTTP.new(uri, p_user='qa@pixotech.com', p_pass='P9cTntdXK2')
-# res.start
-# res = Net::HTTP.post_form(uri, 'url' => 'http://www.google.com', 'submit' => 'Evaluate' )
-# p res
-
-# req = Net::HTTP::Post.new('https://fae.disability.illinois.edu/')
-
-# req.set_form_data({
-#   'url'    => 'http://www.google.com',
-#   'submit' => 'Evaluate'
-#   })
-# res = Net::HTTP.new(uri.host, uri.port, p_user='qa@pixotech.com', p_pass='P9cTntdXK2').start { |http| http.request(req) }
-# p res
-# File.open('out.htm', 'w') { |f| f.write res.body }
-
-# require_relative 'email_reports'
-
-# # class AccessibilityValidator
-
-# # end
-
-# title = 'test'
-# # # Chrome browser instantiation
-# driver = Selenium::WebDriver.for :chrome
-
-# # # Navigate to accessibility checker
-# driver.navigate.to "http://fae20.cita.illinois.edu/login/"
-
-# driver.find_element(:id, 'id_username').send_keys('pixo.qa')
-# driver.find_element(:id, 'id_password').send_keys('P9cTntdXK2')
-# driver.find_element(:css, "input[type='submit']").click
-
-# # # Enter info and click
-# driver.find_element(:id, 'id_input_url').send_keys('www.michaeldburke.net')
-# driver.find_element(:id, 'id_input_title').send_keys(title)
-# driver.find_element(:id, 'depth_1').click
-# driver.find_element(:id, 'id_submit').click
-
-# wait = Selenium::WebDriver::Wait.new(timeout: 60)
-# begin
-#   element = wait.until { driver.find_element(:link_text => title) }
-# end
-# driver.find_element(:link_text => 'Archived Reports').click
-file_contents = open('http://fae20.cita.illinois.edu/report/15758eb303059acb/json/') {|f| f.read}
-File.open('logs/test.json', 'w') { |file| file.write(JSON.pretty_generate(file_contents)) }
-# driver.find_element(:id, 'validate_file_button').click
-
-# # Wait 10 seconds (will make this smarter later)
-# sleep(20)
-
-# # Quitting the browser
-# driver.quit
-
-# # Email report
-# # email = Email.new('/Users/mike/Downloads/achecker_2016-09-21_20-10-49.pdf')
-# # Mail.defaults do
-# #   delivery_method :smtp, email.options
-# # end
-# # email.send
 
 # # Copy report to logs
 # # logs_dir = Dir.pwd + '/logs'
