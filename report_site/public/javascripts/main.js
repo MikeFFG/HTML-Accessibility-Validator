ab0a380ea76b26d0e1aad32a74b099eb5ec6c880
Michael Burke <mike@pixotech.com>
2016-10-04 13:19:05 -0500

    Update a lot of site functionality

--- report_site/public/javascripts/main.js
+++ report_site/public/javascripts/main.js
@@ -11,5 +11,6 @@ $(function() {
   $tabs.on("click", function() {
     $tabs.removeClass("selected");
     $(this).toggleClass("selected");
+    // window.localStorage.setItem('selected_index', $(this).data("id"))
   });
 });
--- report_site/report_site.rb
+++ report_site/report_site.rb
@@ -6,22 +6,57 @@ require 'json'
 
 ACCESS_VAL_LOC = '/report_site/data/accessibility_validation_sample.json'
 HTML_VAL_LOC = '/report_site/data/html_validation_sample.json'
+ACCESS_VAL_LOC2 = '/logs/accessibility_data.json'
+HTML_VAL_LOC2 = '/logs/html_data.json'
+
+class Data
+
+end
 
 configure do
+  enable :sessions
+  set :session_secret, 'secret'
   set :erb, :escape_html => true
 end
 
+helpers do
+  def get_single_site_access_violations(index)
+    return session[:access_vals_all][index]["page_eval_rule_results"].select do |result|
+      result['fields']['elements_violation'] > 0
+    end
+  end
+
+  def get_single_site_html_violations(index)
+    session[:html_vals_all][index]
+  end
+
+  def read_html_data_from_logs
+    JSON.parse(File.read(Dir.pwd + HTML_VAL_LOC2))
+  end
+
+  def read_access_data_from_logs
+    JSON.parse(File.read(Dir.pwd + ACCESS_VAL_LOC2))
+  end
+end
+
+before do
+  session[:index] ||= 0
+  session[:site_tabs] = ["title1", "title2", "title"]
+  session[:access_vals_all] = read_access_data_from_logs
+  session[:html_vals_all] = read_html_data_from_logs
+end
+
 get '/' do
-  @site_tabs = ["title1", "title2", "title3"]
-  @content = { "somekey" => "somevalue",
-               "somekey1" => "somevalue1",
-               "somekey2" => "somevalue2",
-               "somekey3" => "somevalue3",
-             }
-  @html_val = JSON.parse(File.read(Dir.pwd + HTML_VAL_LOC))
-  @access_val = JSON.parse(File.read(Dir.pwd + ACCESS_VAL_LOC))["page_eval_rule_results"].select do |result|
-                  result['fields']['elements_violation'] > 0
-                end
+  @site_tabs = session[:site_tabs] # temp
+
+  redirect "/1"
+end
+
+get '/:id' do
+  index = params[:id].to_i
+  @site_tabs = session[:site_tabs] # temp
+  @access_val = get_single_site_access_violations(index)
+  @html_val = get_single_site_html_violations(index)
 
   erb :table
 end
