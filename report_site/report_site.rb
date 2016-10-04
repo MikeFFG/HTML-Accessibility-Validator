c91f240692ae0a7c9d3537d3d4eab048e0d20b44
Michael Burke <mike@pixotech.com>
2016-10-04 10:37:26 -0500

    work on parsing the accessibility json in test.rb. Add features to reporting site.

--- report_site/report_site.rb
+++ report_site/report_site.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require "sinatra/content_for"
 require "sinatra/reloader"
 require "tilt/erubis"
+require 'json'
 
 configure do
   set :erb, :escape_html => true
@@ -14,6 +15,7 @@ get '/' do
                "somekey2" => "somevalue2",
                "somekey3" => "somevalue3",
              }
+  @html_val = JSON.parse(File.read(Dir.pwd + '/report_site/data/html_validation_sample.json'))
 
   erb :table
 end
--- test.rb
+++ test.rb
@@ -3,8 +3,16 @@ require 'json'
 data = File.read('report_site/data/accessibility_validation_sample.json')
 json_data = JSON.parse(data)
 
-
-json_data["ws_eval_rule_results"].each do |result|
-  puts result if result['fields']['pages_violation'] > 0
+filtered = json_data["page_eval_rule_results"].select do |result|
+  result['fields']['elements_violation'] > 0
 end
 
+filtered.each do |result|
+  puts result['fields']['rule'][0]
+  puts result['fields']['rule'][1]
+  puts result['fields']['rule'][3]
+  puts result['fields']['message']
+  puts 'number of violations: ' + result['fields']['elements_violation'].to_s
+  puts
+  puts
+end
